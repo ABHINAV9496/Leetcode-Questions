0714c586d747411d368af32680725bc81dfcc28f
ABHINAV9496 <132831157+ABHINAV9496@users.noreply.github.com>
2026-02-25 09:39:46 +0530

    Time: 0 ms (100%), Space: 12.6 MB (45.73%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def firstPalindrome(self, words):
+        for x in words:
+            if x==x[::-1]:
+                return x
+        return ""        
+     
+        
